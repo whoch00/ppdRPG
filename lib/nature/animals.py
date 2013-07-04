a1cc8bdf7a91a3be9c893c91ce0e39ca5675a108
a442 <alef321@gmail.com>
2013-07-04 05:18:59 -0300

    Started the Human class.

--- lib/nature/animals.py
+++ lib/nature/animals.py
@@ -5,5 +5,27 @@ from ppdRPG import configs
 from ppdRPG.lib.basic import Npc, Player
 
 class Human(_Object, Npc, Player):
-	"""Human, can be either an Npc or a Player."""
-	# Re-write
+	"""Human, can be either an Npc or a Player.
+	
+	baseType_ is a class, not a string.
+	baseTypeArgs can be either a dictionary or a list or arguments.
+	"""
+	
+	
+	def __init__(self, gender, baseType, baseTypeArgs):
+		
+		if not baseType in [Npc, Player]:
+			raise Exception('Invalid type.')
+		else:
+			self.baseType = baseType
+			self.baseTypeArgs = baseTypeArgs
+		
+		if not gender in ['male','female']:
+			raise Exception('Invalid gender.')
+		else:
+			self.gender = gender
+		
+		try:
+			self.baseType.__init__(self, **baseTypeArgs)
+		except AttributeError:
+			self.baseType.__init__(self, *baseTypeArgs)
