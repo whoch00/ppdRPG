412ff450a9ac18fc083de058243cc6c4fa503d29
a442 <alef321@gmail.com>
2013-07-04 21:19:52 -0300

    Created the Group class, minor adjustments.

--- lib/basic.py
+++ lib/basic.py
@@ -16,11 +16,10 @@ class Place(_Object):
 				bans=None, extraArgs=None, autoSave=configs.autoSave):
 		
 		if name == None:
-			name = instanceName
+			self.name = instanceName
 		self.type_ = Place
 		self.instanceName = instanceName
 		self.verifyDoubleEntry()
-		self.name = name
 		
 		if bans == None:
 			self.bans = []
@@ -46,30 +45,30 @@ class Place(_Object):
 				for j in xrange(0,self.y):
 					self.matrix[i].append(False)
 
-		_Object.__init__(self,extraArgs)
+		_Object.__init__(self, self.extraArgs)
 		
-	def ban(self, namesList):
+	def ban(self, list_):
 		"""Add an object to the place's ban list, 
 		forbidding it to be moved to the place.
 		"""
-		
-		for i in namesList:
+
+		for i in list_:
 			if i in self.bans:
 				pass
 			else:
 				self.bans.append(i)
-	
-	def allow(self, namesList):
+
+	def allow(self, list_):
 		"""Remove a name from the place's ban list, 
 		allowing it to be moved to the place.
 		"""
-		
-		for i in namesList:
+
+		for i in list_:
 			try:
 				self.bans.remove(i)
 			except ValueError:
 				pass
-	
+
 	def availablePositions(self):
 		"""Returns a list of unoccupied indexes of self.matrix"""
 		
@@ -119,11 +118,10 @@ class Npc(_Object, Mobile):
 				autoSave=configs.autoSave):
 		
 		if name == None:
-			name = instanceName
+			self.name = instanceName
 		self.type_ = Npc
 		self.instanceName = instanceName
 		self.verifyDoubleEntry()
-		self.name = name
 		
 		if position == None:
 			self.position = [0,0]
@@ -138,7 +136,7 @@ class Npc(_Object, Mobile):
 		# Saves if autoSave = True (because the place also needs to be updated).
 		self.move(place,position)
 		
-		_Object.__init__(self,extraArgs)
+		_Object.__init__(self, self.extraArgs)
 
 	def say(self,msg):
 		"""Say something."""
@@ -154,27 +152,97 @@ class Npc(_Object, Mobile):
 		return self.say(msg)
 
 class Thing(_Object, Mobile):
-	"""An object (thing)."""
+	"""An object (thing).
+	
+	This class does not inherits the Usable one. If you want an usable thing,
+	use one provided in the library or create your own, since different things
+	have different uses.
+	"""
 	
 	
-	def __init__(self,name,place,position=[0,0]):
+	def __init__(self, instanceName, place, name=None, 
+				position=None, extraArgs=None):
 		
+		if name == None:
+			self.name = instanceName
 		self.type_ = Thing
 		self.instanceName = instanceName
 		self.verifyDoubleEntry()
 		
 		self.place = place
-		self.position = position
+		if position == None:
+			self.position = [0,0]
+		else:
+			self.position = position
+		if extraArgs == None:
+			self.extraArgs = {}
+		else:
+			self.extraArgs = extraArgs
+		
+		_Object.__init__(self, self.extraArgs)
 
 class Player(_Object, Mobile):
 	"""A player (user-controled)."""
 	
 	
-	def __init__(self,name,place,position=[0,0]):
+	# To be done in the future.
+
+class Group(_Object):
+	"""A group of instances with special privileges.
+	
+	This is a special Class in the basic.py file, it does not accept extraArgs
+	and is only used to group instances of other classes into groups with
+	special privileges or duties, according to the developer's choice.
+	"""
+	
+	def __init__(self, instanceName, name=None, members=None, 
+				bans=None, autoSave=configs.autoSave):
 		
-		self.type_ = Player
+		self.type_ = Group
 		self.instanceName = instanceName
 		self.verifyDoubleEntry()
 		
-		self.place = place
-		self.position = position
+		self.extraArgs = None
+		
+		if name == None:
+			self.name = instanceName	
+		if members == None:
+			self.members = []
+		else:
+			self.members = members
+		if bans == None:
+			self.bans = []
+			
+		_Object.__init__(self, self.extraArgs)
+		
+	def add(self, instancesList):
+		"""Add a list of instances to the group's member's list."""
+		
+		for i in instancesList:
+			if i in self.bans:
+				print "%s is not allowed in %s. Please issue %s.allow(%s)\
+					first." % (i, self.name, self.name, i)
+			elif i in self.members:
+				print "%s already in %s" % (i, self.name)
+			else:
+				self.members.append(i)
+	
+	def ban(self, instancesList):
+		"""Ban a list of instances from the groups's member's list."""
+		
+		for i in instancesList:
+			if i in self.members:
+				self.members.remove(i)
+			elif i in self.bans:
+				print "%s already banned from %s" % (i, self.name)
+			else:
+				self.bans.append(i)
+				
+	def allow(self, instancesList):
+		"""Allow a list of instances to the groups's member's list."""
+		
+		for i in instancesList:
+			if not i in self.bans:
+				print "%s already allowed in %s" % (i, self.name)
+			else:
+				self.bans.remove(i)
